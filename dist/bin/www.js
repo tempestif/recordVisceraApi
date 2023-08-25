3373570a61ba51da6aa8bc6a16e0f60e9b515e4f
tempest <soccer.i_y@icloud.com>
2023-08-25 14:48:34 +0900

    first commit

---
+++ dist/bin/www.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env node
+"use strict";
+/**
+ * Module dependencies.
+ */
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("../app"));
+const http = __importStar(require("http"));
+const debugModule = __importStar(require("debug"));
+var debug = debugModule.debug("quick-start-express-typescript:server");
+/**
+ * Get port from environment and store in Express.
+ */
+const port = normalizePort(process.env.PORT || "3000");
+app_1.default.set("port", port);
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app_1.default);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+    const nport = parseInt(val, 10);
+    if (isNaN(nport)) {
+        // named pipe
+        return val;
+    }
+    if (nport >= 0) {
+        // port number
+        return nport;
+    }
+    return false;
+}
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error) {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+    const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case "EACCES":
+            console.error(bind + " requires elevated privileges");
+            process.exit(1);
+        case "EADDRINUSE":
+            console.error(bind + " is already in use");
+            process.exit(1);
+        default:
+            throw error;
+    }
+}
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+    function bind() {
+        const addr = server.address();
+        if (addr === null) {
+            return "";
+        }
+        if (typeof addr === "string") {
+            return "pipe " + addr;
+        }
+        if ("port" in addr) {
+            return "port " + addr.port;
+        }
+    }
+    debug("Listening on " + bind());
+}
