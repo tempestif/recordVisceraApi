db5fc940b3ad9a4d9762f0c360120d0e2b05ea77
kbyshrt <oobayasi0712@gmail.com>
2023-10-05 03:28:02 +0900

    [WIP] bristolStoolScles.ts 作成

---
+++ src/consts/db/bristolStoolScales.ts
@@ -0,0 +1,9 @@
+export const BRISTOL_STOOL_SCALES = {
+    spherical: 1,     //コロコロ便
+    hard: 2,          //硬い便
+    slightlyHard: 3,  //やや硬い便
+    normal: 4,        //普通便
+    slightlySoft: 5,  //やや軟らかい便
+    mudLike: 6,       //泥状便
+    watery: 7         //水様便
+} as const
