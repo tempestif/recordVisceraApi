433f508bee432d71add12826fd514af7d9350e60
tempest <soccer.i_y@icloud.com>
2023-10-04 23:37:28 +0900

    初期データを追加するSQLファイル作成 自動化とかはしていない。

---
+++ defaultSQL/abdominalScaleTypes.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+    `abdominal_scale_types` (`id`, `typeName`, `score`)
+VALUES
+    (1, 'なし', 0);
+
+INSERT INTO
+    `abdominal_scale_types` (`id`, `typeName`, `score`)
+VALUES
+    (2, '疑い', 2);
+
+INSERT INTO
+    `abdominal_scale_types` (`id`, `typeName`, `score`)
+VALUES
+    (3, '確実にあり', 5);
