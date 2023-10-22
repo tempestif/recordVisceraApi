4e9a3fd3d4f74beb34f53ffaf7a218009d90b210
tempest <soccer.i_y@icloud.com>
2023-10-23 00:44:54 +0900

    今日の体調の登録APIを作成

--- src/consts/responseConsts/dailyReport.ts
+++ src/consts/responseConsts/dailyReport.ts
@@ -1,3 +1,23 @@
+export const RECORD_DAILY_REPORT = {
+    message: "今日の体調を記録しました。"
+} as const
+
+export const READ_DAILY_REPORT = {
+    message: "今日の体調記録のリストを取得しました。"
+} as const
+
 export const DAILY_REPORT_NOT_FOUND = {
     message: "今日の体調が見つかりません。"
+} as const
+
+export const DAILY_REPORT_ACCESS_FORBIDDEN = {
+    message: "この今日の体調記録は編集できません"
+} as const
+
+export const EDIT_DAILY_REPORT = {
+    message: "今日の体調記録を編集しました。"
+} as const
+
+export const DELETE_DAILY_REPORT = {
+    message: "今日の体調記録を削除しました。"
 } as const
