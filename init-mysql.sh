6b3f58318a003f231a1f1c263907590376a05cf0
tempestif <soccer.i_y@icloud.com>
2023-10-23 01:40:59 +0900

    shの改行コード変更

--- init-mysql.sh
+++ init-mysql.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+docker-compose exec mysql bash -c "chmod 0644 /etc/mysql/conf.d/my.cnf"
 docker-compose exec mysql bash -c "chmod 0775 docker-entrypoint-initdb.d/init-database.sh"
 docker-compose exec mysql bash -c "./docker-entrypoint-initdb.d/init-database.sh"
