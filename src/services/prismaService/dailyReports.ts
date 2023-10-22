dd9edbff5f95bb50edc7f7ea137d100bce231098
tempest <soccer.i_y@icloud.com>
2023-10-23 00:49:55 +0900

    今日の体調の登録APIを作成

--- src/services/prismaService/dailyReports.ts
+++ src/services/prismaService/dailyReports.ts
@@ -59,26 +59,44 @@ export const createDailyReport = async (userId: number, date: Date, recordData: 
 
     // 紐づく記録テーブルを追加
     // 体温
-    await createDailyReportRecordsTable(customizedPrisma.daily_report_Temp, dailyReportId, { result: recordData.temp });
+    if (recordData.temp) {
+        await createDailyReportRecordsTable(customizedPrisma.daily_report_Temp, dailyReportId, { result: recordData.temp });
+    }
     // 体重
-    await createDailyReportRecordsTable(customizedPrisma.daily_report_Weight, dailyReportId, { result: recordData.weight });
+    if (recordData.weight) {
+        await createDailyReportRecordsTable(customizedPrisma.daily_report_Weight, dailyReportId, { result: recordData.weight });
+    }
     // 腹痛
-    await createDailyReportRecordsTable(customizedPrisma.daily_report_Stomachache, dailyReportId, { result: recordData.stomachach });
+    if (recordData.stomachach) {
+        await createDailyReportRecordsTable(customizedPrisma.daily_report_Stomachache, dailyReportId, { result: recordData.stomachach });
+    }
     // 体調
-    await createDailyReportRecordsTable(customizedPrisma.daily_report_Condition, dailyReportId, { conditionScaleId: recordData.condition });
+    if (recordData.condition) {
+        await createDailyReportRecordsTable(customizedPrisma.daily_report_Condition, dailyReportId, { conditionScaleId: recordData.condition });
+    }
     // 関節痛の有無
-    await createDailyReportRecordsTable(customizedPrisma.daily_report_Arthritis, dailyReportId, { result: recordData.arthritis });
+    if (recordData.arthritis) {
+        await createDailyReportRecordsTable(customizedPrisma.daily_report_Arthritis, dailyReportId, { result: recordData.arthritis });
+    }
     // 皮膚病変の有無
-    await createDailyReportRecordsTable(customizedPrisma.daily_report_Skin_Lesions, dailyReportId, { result: recordData.skinLesitions });
+    if (recordData.skinLesitions) {
+        await createDailyReportRecordsTable(customizedPrisma.daily_report_Skin_Lesions, dailyReportId, { result: recordData.skinLesitions });
+    }
     // 眼病変の有無
-    await createDailyReportRecordsTable(customizedPrisma.daily_report_Ocular_Lesitions, dailyReportId, { result: recordData.ocularLesitions });
+    if (recordData.ocularLesitions) {
+        await createDailyReportRecordsTable(customizedPrisma.daily_report_Ocular_Lesitions, dailyReportId, { result: recordData.ocularLesitions });
+    }
     // 肛門病変の有無
-    await createDailyReportRecordsTable(customizedPrisma.daily_report_Anorectal_Lesitions, dailyReportId, {
-        fistula: recordData.anirectalLesitions,
-        others: recordData.anirectalOtherLesitions
-    });
+    if (recordData.anirectalLesitions || recordData.anirectalOtherLesitions) {
+        await createDailyReportRecordsTable(customizedPrisma.daily_report_Anorectal_Lesitions, dailyReportId, {
+            fistula: recordData.anirectalLesitions,
+            others: recordData.anirectalOtherLesitions
+        });
+    }
     // 腹部腫瘤の有無
-    await createDailyReportRecordsTable(customizedPrisma.daily_report_Abdominal, dailyReportId, { abdominal_Scale_TypesId: recordData.abdominal });
+    if (recordData.abdominal) {
+        await createDailyReportRecordsTable(customizedPrisma.daily_report_Abdominal, dailyReportId, { abdominal_Scale_TypesId: recordData.abdominal });
+    }
 
     // NOTE: ここでエラーが出たら取得できなかったという旨のものが投げられる。それでよい？
     // 返却用にもう一度daily_reportを取得。パフォーマンス悪すぎるなら他のやり方を考える
