33633759bcf604932f1e054c5fe22d6169a6e97d
tempestif <soccer.i_y@icloud.com>
2024-04-07 23:14:08 +0900

    throwValidationErrorのテスト記述

--- src/utils/errorHandle/__test__/validate.test.ts
+++ src/utils/errorHandle/__test__/validate.test.ts
@@ -1,22 +1,66 @@
-import {
-  ERROR_BAD_REQUEST,
-  ERROR_BOWEL_MOVEMENT_NOT_FOUND,
-} from "@/consts/responseMessages";
-import { findValidateError } from "@/utils/errorHandle/validate";
-import {
-  BadRequestError,
-  DbRecordNotFoundError,
-} from "@/utils/errorHandle/errors";
+import { ERROR_BOWEL_MOVEMENT_NOT_FOUND } from "@/consts/responseMessages";
+import { DbRecordNotFoundError } from "@/utils/errorHandle/errors";
+import * as validateUtils from "@/utils/errorHandle/validate";
+import { throwValidationError } from "@/utils/errorHandle/validate";
+import { Result, ValidationError } from "express-validator";
 
 describe("findValidateErrorのテスト", () => {
   test("ErrorResponseMessageTypeを渡すと専用のErrorインスタンスが返却される", () => {
     // テスト対象実行
-    const error = findValidateError(ERROR_BOWEL_MOVEMENT_NOT_FOUND.message);
+    const error = validateUtils.findValidateError(
+      ERROR_BOWEL_MOVEMENT_NOT_FOUND.message
+    );
     const throwError = () => {
       throw error();
     };
 
     expect(throwError).toThrow(DbRecordNotFoundError);
     expect(throwError).toThrow(ERROR_BOWEL_MOVEMENT_NOT_FOUND.message);
+  });
+});
+
+describe("throwValidationErrorのエラー", () => {
+  let spyFindValidateError: jest.SpyInstance<() => Error, [message: string]>;
+
+  beforeEach(() => {
+    // テストの前にfindValidateErrorのモックをクリアする
+    jest.clearAllMocks();
+    spyFindValidateError = jest.spyOn(validateUtils, "findValidateError");
+    spyFindValidateError.mockReturnValue(
+      () => new DbRecordNotFoundError("message")
+    );
+  });
+
+  test("errors.isEmptyがfalseのときエラーをthrowする", () => {
+    const mockErrors: Partial<Result<ValidationError>> = {
+      isEmpty: () => false,
+      array: () => [
+        {
+          type: "field",
+          location: "body",
+          path: "mock-path",
+          value: "mock-value",
+          msg: "mock-msg",
+        },
+      ],
+    };
+
+    const test = () => {
+      throwValidationError(mockErrors as Result<ValidationError>);
+    };
+    expect(test).toThrow(new DbRecordNotFoundError("message"));
+    expect(spyFindValidateError).toHaveBeenCalledWith("mock-msg");
+  });
+
+  test("errors.isEmptyがtrueのときは何もしない", () => {
+    const mockErrors: Partial<Result<ValidationError>> = {
+      isEmpty: () => true,
+      array: () => [],
+    };
+
+    const test = () => {
+      throwValidationError(mockErrors as Result<ValidationError>);
+    };
+    expect(test).not.toThrow();
   });
 });
